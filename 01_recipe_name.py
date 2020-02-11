c619cfbfc7319a86808cc48ddfd7cc6495a62b41
SapE <60630338+kimj9537@users.noreply.github.com>
2020-02-12 10:17:35 +1300

    master

---
+++ 01_recipe_name.py
@@ -0,0 +1,11 @@
+# Get's recipe name and checks it it not blank
+
+# Not Blank Function goes here
+
+
+
+# Main Routine goes here
+
+recipe_name = input(" What is the recipe name? ")
+
+print("You are making {}".format(recipe_name))
