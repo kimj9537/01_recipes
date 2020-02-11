0a86dc4bbdbb66bbda505b76029e3c9c5d0a6f6e
SapE <60630338+kimj9537@users.noreply.github.com>
2020-02-12 10:55:14 +1300

    Update 01_recipe_name.py

--- 01_recipe_name.py
+++ 01_recipe_name.py
@@ -1,11 +1,20 @@
 # Get's recipe name and checks it it not blank
 
 # Not Blank Function goes here
+def not_blank(question):
 
+    valid = False
+    while not valid:
+        response = input(question)
+
+        if response == "":
+            continue
+        else:
+            return response
 
 
 # Main Routine goes here
 
-recipe_name = input(" What is the recipe name? ")
+recipe_name = not_blank(" What is the recipe name? ")
 
 print("You are making {}".format(recipe_name))
