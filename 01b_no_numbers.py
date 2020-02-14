dbd30ebc821fe1a1001c1f65988de2eeec3c3fcf
SapE <60630338+kimj9537@users.noreply.github.com>
2020-02-14 14:46:33 +1300

    master

---
+++ 01b_no_numbers.py
@@ -0,0 +1,20 @@
+# Iterates through string...
+
+#ask user for string
+recipe_name = input("What is the recipe name? ")
+
+error = "Your recipe name has numbers in it"
+has_errors = ""
+#look at each character in string and if it's number, complain
+for letter in recipe_name:
+    if __name__ == '__main__':
+        if letter.isdigit()== True:
+          print(error)
+
+
+          break
+
+
+
+  #give user feedback...
+
