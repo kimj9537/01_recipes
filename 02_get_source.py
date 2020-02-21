adc298c7b1030a0f611efb14ee949217133140ea
SapE <60630338+kimj9537@users.noreply.github.com>
2020-02-21 14:49:16 +1300

    component2

--- 02_get_source.py
+++ 02_get_source.py
@@ -1,4 +1,8 @@
-# Get's recipe name and checks it it not blank
+# Get's source of recipe name and checks it it not blank
+
+# To Do
+# Allow users to specifiy a custom error message
+# Allow uesrs to specify whether numbers are allowed
 
 # Not Blank Function goes here
 def not_blank(question, error_msg, num_ok ):
@@ -10,21 +14,23 @@ def not_blank(question, error_msg, num_ok ):
         response = input(question)
         has_errors = ""
 
-        if num_ok != "yes":
-        #look at each character in string and if it's number, complain
-        for letter in response:
-            if letter.isdigit() == True:
-                has_errors = "yes"
-                break
 
-        if response == "":
-            #print(error)
+
+       if num_ok != "yes":
+          #look at each character in string and if it's number, complain
+          for letter in response:
+             if letter.isdigit() == True:
+                 has_errors = "yes"
+                 break
+
+       if response == "":
+             print(error)
+             continue
+       elif has_errors != "":
+             print(error)
             continue
-        elif has_errors != "":
-            print(error)
-            continue
-        else:
-            return response
+       else:
+          return response
 
 
 # Main Routine goes here
@@ -33,4 +39,4 @@ source = not_blank(" Where is the recipe from? ",
                    "The recipe source can't be blank,",
                    "yes")
 
-print("You are making {}".format(recipe_name))
+print("The recipe is from {}".format(source))
