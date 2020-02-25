d7d2ed46f8a7a2c49e025bb82ef39306ddf3643b
SapE <60630338+kimj9537@users.noreply.github.com>
2020-02-26 10:53:18 +1300

    component 3

--- 02_get_source.py
+++ 02_get_source.py
@@ -4,33 +4,31 @@
 # Allow users to specifiy a custom error message
 # Allow uesrs to specify whether numbers are allowed
 
+
 # Not Blank Function goes here
 def not_blank(question, error_msg, num_ok ):
     error = error_msg
-
 
     valid = False
     while not valid:
         response = input(question)
         has_errors = ""
 
+        if num_ok != "yes":
+            # look at each character in string and if it's number, complain
+            for letter in response:
+                if letter.isdigit():
+                    has_errors = "yes"
+                    break
 
-
-       if num_ok != "yes":
-          #look at each character in string and if it's number, complain
-          for letter in response:
-             if letter.isdigit() == True:
-                 has_errors = "yes"
-                 break
-
-       if response == "":
-             print(error)
-             continue
-       elif has_errors != "":
-             print(error)
+        if response == "":
+            print(error)
             continue
-       else:
-          return response
+        elif has_errors != "":
+            print(error)
+            continue
+        else:
+            return response
 
 
 # Main Routine goes here
