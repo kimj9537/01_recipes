b8764a0e2a55595ab591ffcf6584233674cb5ace
SapE <60630338+kimj9537@users.noreply.github.com>
2020-04-17 14:41:39 +1200

    component 7

--- 06_convert_mls.py
+++ 06_convert_mls.py
@@ -6,22 +6,67 @@
 
 # if no unit given / unit is unknown, leave as is.
 
+
+# ***** Functions go here *****
+def unit_checker():
+
+    unit_tocheck = input("Unit? ")
+
+    # Abbreviation lists
+    teaspoon = ["tsp", "teaspoon", "t"]
+    tablespoon = ["tbs", "tablespoon", "T", "tbsp"]
+    ounce = ["oz", "fluid", "ounce", "fl", "oz"]
+    cup = ["c"]
+    pint = ["p", "pt", "qt", "flpt"]
+    quart = ["q", "qt", "flqt"]
+    ml = ["milliliter", "millilitre", "mL"]
+    l = ["liter", "litre", "L"]
+    dl = ["deciliter", "decilitre", "dL"]
+    pound = ["lb", "#"]
+
+    if unit_tocheck == "":
+        print("you chose {}".format(unit_tocheck))
+        return unit_tocheck
+
+    elif unit_tocheck == "T" or unit_tocheck.lower() in tablespoon:
+        return "tbs"
+    elif unit_tocheck.lower() in teaspoon:
+        return "tsp"
+    elif unit_tocheck.lower() in ounce:
+        return "ounce"
+    elif unit_tocheck.lower() in cup:
+        return "cup"
+    elif unit_tocheck.lower() in pint:
+        return "pint"
+    elif unit_tocheck.lower() in quart:
+        return "quart"
+    elif unit_tocheck.lower() in ml:
+        return "ml"
+    elif unit_tocheck.lower() in l:
+        return "l"
+    elif unit_tocheck.lower() in dl:
+        return "dl"
+    elif unit_tocheck.lower() in pound:
+        return "pound"
+
+# ****** Main Routine Goes Here ********
 unit_central = {
     "tsp": 5,
     "tbs": 15,
     "cup": 237,
-    "ounce": 30,
+    "ounce": 28.35,
     "pint": 473,
     "quart": 946,
-    "pound": 454
+    "pound": 454,
+    "litre": 1000
 }
 keep_going = ""
 while keep_going == "":
     amount = eval(input("How much? "))
     amount = float(amount)
 
-
-    unit = input("Unit? ")
+    # Get unit and change it to match dictionary.
+    unit = unit_checker()
 
     if unit in unit_central:
         mult_by = unit_central.get(unit)
