32c5259c21eb56ecb00d8cd337325085da03f3ce
SapE <60630338+kimj9537@users.noreply.github.com>
2020-04-20 13:49:26 +1200

    component 9

---
+++ 08_converter.py
@@ -0,0 +1,76 @@
+# Conversation Function...
+
+
+# ***** Functions go here *****
+def general_converter(how_much, lookup, dictionary, conversation_factor):
+
+    if lookup in dictionary:
+        mult_by = dictionary.get(unit)
+        how_much = how_much * mult_by * conversation_factor
+
+    return how_much
+
+def unit_checker():
+
+    unit_tocheck = input("Unit? ")
+
+    # Abbreviation lists
+    teaspoon = ["tsp", "teaspoon", "t"]
+    tablespoon = ["tbs", "tablespoon", "T", "tbsp"]
+    ounce = ["oz", "fluid", "ounce", "fl", "oz"]
+    cup = ["c"]
+    pint = ["p", "pt", "qt", "flpt"]
+    quart = ["q", "qt", "flqt"]
+    ml = ["milliliter", "millilitre", "mL"]
+    l = ["liter", "litre", "L"]
+    dl = ["deciliter", "decilitre", "dL"]
+    pound = ["lb", "#"]
+
+    if unit_tocheck == "":
+        print("you chose {}".format(unit_tocheck))
+        return unit_tocheck
+
+    elif unit_tocheck == "T" or unit_tocheck.lower() in tablespoon:
+        return "tbs"
+    elif unit_tocheck.lower() in teaspoon:
+        return "tsp"
+    elif unit_tocheck.lower() in ounce:
+        return "ounce"
+    elif unit_tocheck.lower() in cup:
+        return "cup"
+    elif unit_tocheck.lower() in pint:
+        return "pint"
+    elif unit_tocheck.lower() in quart:
+        return "quart"
+    elif unit_tocheck.lower() in ml:
+        return "ml"
+    elif unit_tocheck.lower() in l:
+        return "l"
+    elif unit_tocheck.lower() in dl:
+        return "dl"
+    elif unit_tocheck.lower() in pound:
+        return "pound"
+
+# ****** Main Routine Goes Here ********
+unit_central = {
+    "tsp": 5,
+    "tbs": 15,
+    "cup": 237,
+    "ounce": 28.35,
+    "pint": 473,
+    "quart": 946,
+    "pound": 454,
+    "litre": 1000
+}
+keep_going = ""
+while keep_going == "":
+    amount = eval(input("How much? "))
+    amount = float(amount)
+
+    # Get unit and change it to match dictionary.
+    unit = unit_checker()
+
+    amount = general_converter(amount, unit, unit_central, 1)
+    print(amount)
+
+    #keep_going = input("<enter> or q ")
